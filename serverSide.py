468413f6bb837cc667144d778d5e4a411930627f
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2022-01-16 05:42:49 +0500

    Update serverSide.py

--- serverSide.py
+++ serverSide.py
@@ -28,7 +28,8 @@ print("GPU is", "available" if tf.config.experimental.list_physical_devices("GPU
 
 model =load_model('fully_trained.h5')
 print('Model Loaded')
-
+outputfile='output.png'
+savePath='./output/'
 def prepare_image(image,target):
     if image.mode !="RGB":
         image=image.convert('RGB')
@@ -38,7 +39,14 @@ def prepare_image(image,target):
     image=(image-127.5)/127.5
     image =np.expand_dims(image,axis=0)
     return image
+def predict_image(prepared_image):
+    preds=model.predict(prepared_image)
 
+   
+
+    output=tf.reshape(preds,[256,256,3])
+    output = (output+1)/2
+    save_img(savePath+outputfile,img_to_array(output))
 class Predict(Resource):
     def post(self):
         json_data=request.get_json()
@@ -47,28 +55,23 @@ class Predict(Resource):
         image=base64.b64decode(str(img_data))
         
         img=Image.open(io.BytesIO(image))
-        
+        encoded_string=[]
         prepared_image=prepare_image(img,target=(256,256))
+        for x in range(4):
+            predict_image(prepared_image)
+            imageNew=Image.open(savePath+outputfile)
+            imageNew=imageNew.resize((50,50))
+            imageNew.save(savePath+'new_'+x+outputfile)
 
-        preds=model.predict(prepared_image)
-
-        outputfile='output.png'
-        savePath='./output/'
-
-        output=tf.reshape(preds,[256,256,3])
-        output = (output+1)/2
-        save_img(savePath+outputfile,img_to_array(output))
-
-        imageNew=Image.open(savePath+outputfile)
-        imageNew=imageNew.resize((50,50))
-        imageNew.save(savePath+'new_'+outputfile)
-
-        with open(savePath+'new_'+outputfile,'rh') as image_file:
-            encoded_string=base64.b64encode(image_file.read())
+            with open(savePath+'new_'+x+outputfile,'rh') as image_file:
+                encoded_string[x]=base64.b64encode(image_file.read())
 
         outputData={
-            'image':str(encoded_string),
-            
+            'image1':str(encoded_string[0]),
+            'image2':str(encoded_string[1]),
+            'image3':str(encoded_string[2]),
+            'image4':str(encoded_string[3]),
+
         }
         return outputData
 
